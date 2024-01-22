46680e56803461a3e677ce62fa4c32e80c91938a
meliioko <mathieu.latournerie@epita.fr>
2024-01-22 22:21:38 +0100

    removed dead cide

--- backend/app.py
+++ backend/app.py
@@ -31,25 +31,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/api/upload', methods=['POST'])
-def upload_file():
-    # Check if the post request has the file part
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part"})
-
-    file = request.files['file']
-
-    # If the user does not select a file, the browser submits an
-    # empty file without a filename.
-    if file.filename == '':
-        return jsonify({"error": "No selected file"})
-
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return jsonify({"message": "File uploaded successfully", "filename": filename})
-
-    return jsonify({"error": "Invalid file type"})
 
 path = 'dbfs:/databricks/mlflow-tracking/2713494469571469/672969b1bd224ad49cd2c0853ea51a80/artifacts/model/'
 test = mlflow.pytorch.load_model(path)
